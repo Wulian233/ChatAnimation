d7c20b1d7957d0a34b6a7218b8bfd53aaf9d07cb
Wulian <1055917385@qq.com>
2024-10-11 19:35:50 +0800

    Remove Forge Support

--- forge/src/main/java/com/wulian/chatimpressiveanimation/forge/ChatImpressiveAnimationExpectPlatformImpl.java
+++
@@ -1,9 +0,0 @@
-package com.wulian.chatimpressiveanimation.forge;
-
-import com.wulian.chatimpressiveanimation.forge.loader.api.ForgifiedLoader;
-
-public class ChatImpressiveAnimationExpectPlatformImpl {
-	public static Object getObjectShareItem(String key) {
-		return ForgifiedLoader.getInstance().getObjectShare().get(key);
-	}
-}
